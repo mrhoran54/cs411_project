7f5674fb3ca878f789d1eff06762ad580f5b35df
zehuaz <zehuaz@bu.edu>
2017-05-03 14:15:39 -0400

    html editted

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+CSRF_ENABLED = True
+DEBUG = True
+SECRET_KEY = "345435dsmd.35##3f90ec8062a9e91707e70c2edb919f7e8236ddb5"
+SESSION_TYPE ='filesystem'
+
+#yelp
+CLIENT_ID = "8_f3OeYk3Xet1hRoUrq2qQ"
+CLIENT_SECRET = "IBcbN6KYSxCQp7GOwRKkdRJ1087wosdTbrjKHgif31voikQXzXNxwCxnYs1anCtt"
+
+#facebook
+FACEBOOK_APP_ID = '426344891059039'
+FACEBOOK_APP_SECRET = 'a9afa0f63af2cbc8385f7daaa9ca21dc'
+
+# google maps
+GOOGLEMAPS_KEY = "AIzaSyDPIxQ95g3W-PAd0WPy_PjM84-HtAKQp1U"
